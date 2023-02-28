346992a052d4b7bb8028eaee51b4f29ae2ad98be
iamakeem <bolarinwa_akeem@yahoo.com>
2023-02-28 15:25:33 +0100

    finishing up

--- src/Card.jsx
+++ src/Card.jsx
@@ -7,6 +7,7 @@ import {
   Img,
   SimpleGrid,
   Icon,
+  Spacer,
 } from '@chakra-ui/react';
 import { AiOutlineLike } from 'react-icons/ai';
 
@@ -26,7 +27,7 @@ function Card() {
               <Img
                 borderRadius='full'
                 boxSize='40px'
-                src='https://bit.ly/dan-abramov'
+                src='./images/elephant.png'
                 alt='Dan Abramov'
               />
             </Box>
@@ -55,18 +56,14 @@ function Card() {
             </Flex>
           </Flex>
         </Box>
-        <Box border='2px solid gray' borderRadius='6px' h='400px' w='320px'>
-          <Img
-            boxSize='320px'
-            src='https://bit.ly/dan-abramov'
-            alt='Dan Abramov'
-          />
+        <Box border='1px solid #E5E5E5' borderRadius='6px' h='400px' w='320px'>
+          <Img boxSize='320px' src='./images/elephant.png' alt='Dan Abramov' />
           <Flex>
             <Box>
               <Img
                 borderRadius='full'
                 boxSize='40px'
-                src='https://bit.ly/dan-abramov'
+                src='./images/elephant.png'
                 alt='Dan Abramov'
               />
             </Box>
@@ -95,44 +92,62 @@ function Card() {
             </Flex>
           </Flex>
         </Box>
-        <Box border='2px solid gray' borderRadius='6px' h='276px' w='428px'>
-          <Img
-            w='428px'
-            h='216px'
-            src='https://bit.ly/dan-abramov'
-            alt='Dan Abramov'
-          />
-          <Flex>
+        <Box border='1px solid #E5E5E5' borderRadius='6px' h='276px' w='428px'>
+          <Img w='428px' h='216px' src='./images/ocean.png' alt='Dan Abramov' />
+          <Flex pl='10px' pr='20px' alignItems='center'>
             <Box>
               <Img
                 borderRadius='full'
                 boxSize='40px'
-                src='https://bit.ly/dan-abramov'
+                src='./images/ocean.png'
                 alt='Dan Abramov'
               />
             </Box>
-            <VStack>
-              <Text
-                color='#4F4F4F'
-                fontSize='md'
-                fontWeight='700'
-                fontFamily='Montserrat'
-              >
-                Julia Robertson
-              </Text>
-              <Text
-                as='i'
-                color='#A7A7A7'
-                fontSize='10px'
-                fontWeight='600'
-                fontFamily='Poppins'
-              >
-                @juliaclicks
-              </Text>
-            </VStack>
-            <Flex justifyContent='center' alignItems='center' gap='2'>
-              <AiOutlineLike />
-              <Text> 2.3k</Text>
+            <Flex
+              w='140px'
+              h='32px'
+              ml='10px'
+              direction='column'
+              justifyContent='sapce-around'
+              alignItems='flex-start'
+            >
+              <Box>
+                <Text
+                  color='#4F4F4F'
+                  fontSize='md'
+                  fontWeight='700'
+                  fontFamily='Montserrat'
+                >
+                  Julia Robertson
+                </Text>
+              </Box>
+              <Box>
+                <Text
+                  as='i'
+                  color='#A7A7A7'
+                  fontSize='10px'
+                  fontWeight='600'
+                  fontFamily='Poppins'
+                >
+                  @juliaclicks
+                </Text>
+              </Box>
+            </Flex>
+            <Spacer />
+            <Flex alignItems='center' justifyContent='center'>
+              <Box>
+                <AiOutlineLike />
+              </Box>
+              <Box>
+                <Text
+                  color='#4F4F4F'
+                  fontSize='10px'
+                  fontWeight='700'
+                  fontFamily='Montserrat'
+                >
+                  2.3k
+                </Text>
+              </Box>
             </Flex>
           </Flex>
         </Box>
